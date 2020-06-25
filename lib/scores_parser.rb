f1f561e00a22a015576260934be508c91fdd837e
Rafael Lustosa Mancini <rafael.lmancini@gmail.com>
2020-06-24 21:32:34 -0300

    Add shcanges to score_parser

--- lib/scores_parser.rb
+++ lib/scores_parser.rb
@@ -4,39 +4,71 @@ class ScoresParser
   end
 
   def parse
+    @formatted_scores = {}
     # Replace invalid scores and fouls for 0
-    normalize_score = lambda { |el| el[:score].to_i.negative? ? el.merge!(value: 0) : el.merge!(value: el[:score].to_i) }
-    @scores.each(&normalize_score)
+    evaluate_score = lambda { |el| el[:score].to_i.negative? ? el.merge!(value: 0) : el.merge!(value: el[:score].to_i) }
+    @scores.each(&evaluate_score)
     # Split scores by players
     @scores = @scores.group_by{ |el| el[:player] }
-    # Check if scores are valid (will raise errors if not)
-    check_scores_validity
+    # Populate structured scores (will raise errors if scores are invalid)
+    parse_scores
     # Return structured scores
-    @scores
+    @formatted_scores
   end
 
   private
 
-  def check_scores_validity
+  def populate_structured_scores(fn:, pins:, scores:)
+    @formatted_scores[@player]["frame_#{fn}"] = 
+      { pinfalls: pins, scores: scores }
+  end
+
+  def parse_scores
     @scores.each do |score|
-      player, player_scores = score
+      # Create structured score for player
+      @player, player_scores = score
+      @formatted_scores[@player] = {}
+
+      # Run through player scores populating information
       current_frame = 0
       number_of_frames = 0
       while current_frame < player_scores.count
-        raise "Too many frames in card for player #{player}" if (number_of_frames+1) > 10
-        current_score = player_scores[current_frame][:value]
-        next_score = player_scores&.[](current_frame+1)&.dig(:value).to_i
-        if current_score == 10 # Strike
-          current_frame += 1
+        current_score_value = player_scores[current_frame][:value]
+        current_score = player_scores[current_frame][:score]
+        next_score_value = player_scores&.[](current_frame+1)&.dig(:value).to_i
+        next_score = player_scores&.[](current_frame+1)&.dig(:score)
+        if number_of_frames < 9
+          if current_score_value == 10 # Strike
+            current_frame += 1
+            number_of_frames += 1
+            populate_structured_scores(fn: number_of_frames, pins: ["", "X"], scores: [current_score_value, 0])
+            next
+          elsif current_score_value + next_score_value == 10 # Spare
+            current_frame += 2
+            number_of_frames += 1
+            populate_structured_scores(fn: number_of_frames, pins: [current_score, "/"], scores: [current_score_value, next_score_value])
+            next
+          elsif current_score_value + next_score_value < 10 # Foul/Miss
+            current_frame += 2
+            number_of_frames += 1
+            populate_structured_scores(fn: number_of_frames, pins: [current_score, next_score], scores: [current_score_value, next_score_value])
+            next
+          end
+        else # 10th frame
+          current_score = current_score_value == 10 ? "X" : current_score
+          next_score = next_score_value == 10 ? "X" : next_score
+          extra_score_value = player_scores&.[](current_frame+2)&.dig(:value).to_i
+          extra_score = player_scores&.[](current_frame+2)&.dig(:score)
+          extra_score = extra_score_value == 10 ? "X" : extra_score
+          current_frame += 3
           number_of_frames += 1
-          next
-        elsif current_score + next_score <= 10 # Spare/Foul/Miss
-          current_frame += 2
-          number_of_frames += 1
+          populate_structured_scores(fn: number_of_frames,
+                                     pins: [current_score, next_score, extra_score],
+                                     scores: [current_score_value, next_score_value, extra_score_value])
           next
         end
-        raise "Invalid scores in the card for player #{player} on frame #{current_frame+1}" if current_score > 10
-        raise "Invalid sum of scores for player #{player} on frame #{current_frame+1}" if current_score + next_score > 10
+        raise "Invalid scores in the card for player #{@player} on frame #{current_frame+1}" if current_score_value > 10
+        raise "Invalid sum of scores for player #{@player} on frame #{current_frame+1}" if current_score_value + next_score_value > 10
       end
     end
   end
--- spec/lib/scores_parser_spec.rb
+++ spec/lib/scores_parser_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require_relative "../../lib/score_card_reader"
+require_relative "../../lib/scores_parser"
 
 describe ScoresParser do
   it "can be instantiated" do
@@ -21,11 +21,6 @@ describe ScoresParser do
         parser = ScoresParser.new(scores: fake_scores)
         expect { parser.parse }.to raise_error(RuntimeError, "Invalid sum of scores for player Jeff on frame 1")
       end
-      it "raises errors for more than 10 frames" do
-        fake_scores = Array.new(11, { player: "Jeff", score: "10"} )
-        parser = ScoresParser.new(scores: fake_scores)
-        expect { parser.parse }.to raise_error(RuntimeError, "Too many frames in card for player Jeff")
-      end
     end
     context "when handling valid data" do
       it "returns structured score information" do
@@ -33,11 +28,14 @@ describe ScoresParser do
           { player: "Jeff", score: "10" },
           { player: "John", score: "8" },
           { player: "John", score: "2" },
+
           { player: "Jeff", score: "6" },
           { player: "Jeff", score: "3" },
           { player: "John", score: "10" },
+
           { player: "Jeff", score: "10" },
           { player: "John", score: "10" },
+
           { player: "Jeff", score: "F" },
           { player: "Jeff", score: "F" },
           { player: "John", score: "F" },
@@ -48,8 +46,8 @@ describe ScoresParser do
         expect(scores).not_to be_empty
         expect(scores["Jeff"]).not_to be_empty
         expect(scores["John"]).not_to be_empty
-        expect(scores["Jeff"].length).to eq(6)
-        expect(scores["John"].length).to eq(6)
+        expect(scores["Jeff"].length).to eq(4)
+        expect(scores["John"].length).to eq(4)
       end
     end
   end
